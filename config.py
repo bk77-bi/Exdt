9f931e34ec793ccea9dc4db60e113cd843fa78fd
bk77-bi <bk7738702@gmail.com>
2025-07-28 19:55:42 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,39 @@
+import os
+from os import getenv
+
+
+
+# ------------------------------------------------
+API_ID = int(os.environ.get("API_ID", "26468828"))
+# ------------------------------------------------
+API_HASH = os.environ.get("API_HASH","4693513c08d1ac6af15f95b116c29478")
+# ------------------------------------------------
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+# ------------------------------------------------
+BOT_USERNAME = os.environ.get("BOT_USERNAME", "@Team_JNC_Robot")
+BOT_TEXT = "Beast Extractor"
+# ------------------------------------------------
+OWNER_ID = int(os.environ.get("OWNER_ID", "7517045929"))
+# ------------------------------------------------
+# //LOG CHANNEL ID 
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002730067288"))
+
+# //FORCE_CHANNEL_ID
+CHANNEL_ID2 = int(os.environ.get("CHANNEL_ID2", "-1002730067288")) 
+# ------------------------------------------------
+MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://chiruedizz:WmzSiQlS35fLDImn@cluster0.4o4zl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+# -----------------------------------------------
+PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-1002780495855"))
+# -----------------------------------------------
+join = '<a href="https://t.me/jnc_devoloper">✳️ JOIN BACKUP</a>'
+# -----------------------------------------------
+UNSPLASH_ACCESS_KEY = 'RabDRmuXXBobanmwwbvpP5LwoG4J8ox34y5Sstz-9jk'
+# -----------------------------------------------
+UNSPLASH_QUERY = 'animal baby'
+# -----------------------------------------------
+ADMIN_BOT_USERNAME = "dc5txt_bot" #without @
+
+THUMB_URL = os.environ.get("THUMB_URL", "https://graph.org/file/6307bea2182f82424bf90-e2f24f59f49a97b6c8.jpg")
+thumb_url = os.environ.get("thumb_url","https://graph.org/file/6307bea2182f82424bf90-e2f24f59f49a97b6c8.jpg")
+CONTACT = os.environ.get("CONTACT","[TEAM_JNC](https://t.me/jnc_devoloper)")
+CREDIT = os.environ.get("CREDIT","Team_jnc") #DONT PASTE LINK ONLY BEAST / CHIRU
