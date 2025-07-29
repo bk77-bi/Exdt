2440efb84f4006bceeeb58048f84da38c0f92658
bk77-bi <bk7738702@gmail.com>
2025-07-29 07:09:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ API_ID = int(os.environ.get("API_ID", "20114039"))
 # ------------------------------------------------
 API_HASH = os.environ.get("API_HASH","87297b8f3cc8fc9bbce591ad30da5896")
 # ------------------------------------------------
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7753980767:AAHFxOznb7lD8RlmI-ujX1pEKGwnZQm1NBg")
 # ------------------------------------------------
 BOT_USERNAME = os.environ.get("BOT_USERNAME", "@Bhumihar_EXT_bot")
 BOT_TEXT = "Bhumihar Extractor"
@@ -16,14 +16,14 @@ BOT_TEXT = "Bhumihar Extractor"
 OWNER_ID = int(os.environ.get("OWNER_ID", "8172163893"))
 # ------------------------------------------------
 # //LOG CHANNEL ID 
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002730067288"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002614908937"))
 
 # //FORCE_CHANNEL_ID
-CHANNEL_ID2 = int(os.environ.get("CHANNEL_ID2", "-1002730067288")) 
+CHANNEL_ID2 = int(os.environ.get("CHANNEL_ID2", "-1002500606405")) 
 # ------------------------------------------------
 MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://chiruedizz:WmzSiQlS35fLDImn@cluster0.4o4zl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 # -----------------------------------------------
-PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-1002780495855"))
+PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-1002633965239"))
 # -----------------------------------------------
 join = '<a href="https://t.me/BHUMIHARBOTSS">✳️ JOIN BACKUP</a>'
 # -----------------------------------------------
