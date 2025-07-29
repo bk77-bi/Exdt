b546e85913a35c3a95068a8519b2e3fbae7d1902
bk77-bi <bk7738702@gmail.com>
2025-07-29 06:28:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,16 +4,16 @@ from os import getenv
 
 
 # ------------------------------------------------
-API_ID = int(os.environ.get("API_ID", "26468828"))
+API_ID = int(os.environ.get("API_ID", "20114039"))
 # ------------------------------------------------
-API_HASH = os.environ.get("API_HASH","4693513c08d1ac6af15f95b116c29478")
+API_HASH = os.environ.get("API_HASH","87297b8f3cc8fc9bbce591ad30da5896")
 # ------------------------------------------------
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 # ------------------------------------------------
-BOT_USERNAME = os.environ.get("BOT_USERNAME", "@Team_JNC_Robot")
-BOT_TEXT = "Beast Extractor"
+BOT_USERNAME = os.environ.get("BOT_USERNAME", "@Bhumihar_EXT_bot")
+BOT_TEXT = "Bhumihar Extractor"
 # ------------------------------------------------
-OWNER_ID = int(os.environ.get("OWNER_ID", "7517045929"))
+OWNER_ID = int(os.environ.get("OWNER_ID", "8172163893"))
 # ------------------------------------------------
 # //LOG CHANNEL ID 
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002730067288"))
@@ -25,15 +25,15 @@ MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://chiruedizz:WmzSiQlS35fLDI
 # -----------------------------------------------
 PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-1002780495855"))
 # -----------------------------------------------
-join = '<a href="https://t.me/jnc_devoloper">✳️ JOIN BACKUP</a>'
+join = '<a href="https://t.me/BHUMIHARBOTSS">✳️ JOIN BACKUP</a>'
 # -----------------------------------------------
 UNSPLASH_ACCESS_KEY = 'RabDRmuXXBobanmwwbvpP5LwoG4J8ox34y5Sstz-9jk'
 # -----------------------------------------------
 UNSPLASH_QUERY = 'animal baby'
 # -----------------------------------------------
-ADMIN_BOT_USERNAME = "dc5txt_bot" #without @
+ADMIN_BOT_USERNAME = "Thebhumihar" #without @
 
 THUMB_URL = os.environ.get("THUMB_URL", "https://graph.org/file/6307bea2182f82424bf90-e2f24f59f49a97b6c8.jpg")
 thumb_url = os.environ.get("thumb_url","https://graph.org/file/6307bea2182f82424bf90-e2f24f59f49a97b6c8.jpg")
-CONTACT = os.environ.get("CONTACT","[TEAM_JNC](https://t.me/jnc_devoloper)")
-CREDIT = os.environ.get("CREDIT","Team_jnc") #DONT PASTE LINK ONLY BEAST / CHIRU
+CONTACT = os.environ.get("CONTACT","[Bhumihar](https://t.me/Thebhumihar)")
+CREDIT = os.environ.get("CREDIT","Bhumihar") #DONT PASTE LINK ONLY BEAST / CHIRU
